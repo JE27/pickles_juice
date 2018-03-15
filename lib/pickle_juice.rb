b43adee5603400c3eafc0b6b2c9c4602b29fb4a6
je27 <jocmoya@gmail.com>
2018-03-15 18:21:40 -0500

    initial commit

---
+++ lib/pickle_juice.rb
@@ -0,0 +1,10 @@
+require "pickle_juice/version"
+
+module PickleJuice
+  class Pickles
+    
+    def juice
+      'say oooooo'
+    end
+  end  
+end
---
+++ lib/pickle_juice/version.rb
@@ -0,0 +1,3 @@
+module PickleJuice
+  VERSION = "0.1.0"
+end
